376d50672cb0594e2de04fe286a1e86606d1c356
Sehajpreet Singh <sehajmagan@gmail.com>
2023-10-11 19:05:31 -0400

    updated fcts app

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,32 +1,133 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 
 import "./App.css";
 
 const content = {
   header: {
-    title: "Hacker Stories",
-    secondaryTitle: "Open Forum Board",
+    title: "Human Blabering",
+    secondaryTitle: "Share Cool Facts",
   },
-  Posts: [
+  posts: [
     {
-      title: "How to build a blog app with React and Vite",
-      description: "This is the best tutorial on building a blog app",
-      author: "sehaj",
-      date: "2021-11-3",
-      Id: 1,
-      likes: 0,
-      comments: ["good one", "no one", "i am comment", "am i comment?"],
+      title: "The Mysterious Language of Whales",
+      description:
+        "Whales communicate using complex songs that can travel for hundreds of miles through the ocean. These songs, unique to each group, remain one of the great mysteries of marine biology.",
+      author: "MarineBiologyFan",
+      date: "2023-11-03",
+      id: 1,
+      likes: 15,
+      comments: ["Fascinating!", "Nature's wonders never cease to amaze."],
     },
     {
-      title: "Post 2",
-      description: "Can you learn typescript?",
-      author: "magan",
-      date: "2023-11-3",
-      Id: 2,
-      likes: 0,
-      comments: ["good one", "no one", "i am comment", "am i comment?"],
+      title: "The Science Behind Lightning Strikes",
+      description:
+        "Lightning is a discharge of electricity in the atmosphere. A single lightning bolt can heat the surrounding air to 50,000 degrees Fahrenheit - hotter than the surface of the sun!",
+      author: "StormChaser123",
+      date: "2023-10-25",
+      id: 2,
+      likes: 8,
+      comments: ["Impressive and dangerous!", "Nature's fireworks."],
+    },
+    {
+      title: "The Mystery of Dark Matter",
+      description:
+        "Dark matter makes up about 85% of the universe's total mass, yet it doesn't interact with light or other forms of electromagnetic radiation. Its nature remains one of the greatest puzzles in astrophysics.",
+      author: "CosmicExplorer",
+      date: "2023-09-15",
+      id: 3,
+      likes: 25,
+      comments: [
+        "I hope we find answers soon!",
+        "Dark matter, the enigma of the cosmos.",
+      ],
+    },
+    {
+      title: "The Magic of Bioluminescent Organisms",
+      description:
+        "Bioluminescent organisms, like fireflies and certain deep-sea creatures, produce light through chemical reactions. They create their own natural light shows in the darkness of the natural world.",
+      author: "BioEnthusiast",
+      date: "2023-08-05",
+      id: 4,
+      likes: 12,
+      comments: [
+        "Nature's own fireworks!",
+        "I'd love to see a bioluminescent bay.",
+      ],
+    },
+    {
+      title: "The Surprising World of Ant Colonies",
+      description:
+        "Ant colonies can be massive, housing millions of ants. These tiny insects can organize complex societies, communicate through pheromones, and solve problems collectively.",
+      author: "AntLover23",
+      date: "2023-07-19",
+      id: 5,
+      likes: 9,
+      comments: ["Ants are amazing engineers!", "Nature's little architects."],
+    },
+    {
+      title: "The Unexplained Phenomenon of Ball Lightning",
+      description:
+        "Ball lightning is a rare and poorly understood atmospheric electrical phenomenon. Witnesses have reported seeing luminous, spherical objects that float, flicker, and disappear.",
+      author: "MysteryHunter",
+      date: "2023-06-28",
+      id: 6,
+      likes: 7,
+      comments: [
+        "I've always been intrigued by ball lightning.",
+        "A true scientific mystery.",
+      ],
+    },
+    {
+      title: "The Wonders of Human Memory",
+      description:
+        "The human brain can store information equivalent to about 2.5 petabytes of data, enough to fill over three million hours of TV shows. Our memory's capacity is truly astounding.",
+      author: "MemoryGeek",
+      date: "2023-05-14",
+      id: 7,
+      likes: 17,
+      comments: [
+        "Our brains are like supercomputers!",
+        "Memory is the library of the mind.",
+      ],
+    },
+    {
+      title: "The Intricate World of Spider Silk",
+      description:
+        "Spider silk is not only incredibly strong, but it's also lightweight and elastic. Some spider silk varieties are as strong as steel, and they are used by spiders for hunting and web-building.",
+      author: "ArachnoFanatic",
+      date: "2023-04-02",
+      id: 8,
+      likes: 11,
+      comments: [
+        "Nature's engineers at work!",
+        "Spider silk is a marvel of bioengineering.",
+      ],
+    },
+    {
+      title: "The Enigma of Crop Circles",
+      description:
+        "Crop circles are intricate geometric patterns found in fields, often attributed to mysterious origins. While many are hoaxes, some remain unexplained, fueling conspiracy theories and speculation.",
+      author: "CropCircleEnigma",
+      date: "2023-03-19",
+      id: 9,
+      likes: 6,
+      comments: [
+        "I've always wondered about crop circles.",
+        "Are they messages from extraterrestrial beings?",
+      ],
+    },
+    {
+      title: "The Secret Life of Trees",
+      description:
+        "Trees communicate with each other through a network of fungal mycelia called the 'wood wide web.' They can exchange nutrients and information, helping each other survive and thrive.",
+      author: "TreeHugger",
+      date: "2023-02-11",
+      id: 10,
+      likes: 14,
+      comments: [
+        "Nature's social network!",
+        "We have much to learn from trees.",
+      ],
     },
   ],
 };
@@ -35,7 +136,7 @@ function App() {
     SetDisplaySearchTerm(event.target.value);
   };
   const [displaySearchTerm, SetDisplaySearchTerm] = useState("");
-  const filteredPosts = content.Posts.filter((post) => {
+  const filteredPosts = content.posts.filter((post) => {
     return post.title.toLowerCase().includes(displaySearchTerm.toLowerCase());
   });
   return (
@@ -43,12 +144,16 @@ function App() {
       <Header />
       <hr />
       <div className="d-flex flex-column justify-content-start align-items-start">
-        <Search onSearch={handleSearchTerm} />
+        <Search onSearch={handleSearchTerm} term={displaySearchTerm} />
         {displaySearchTerm && <p>Looking for {displaySearchTerm}</p>}
       </div>
       <br />
-      <div>
-        <Posts Posts={filteredPosts} />
+      <div className="row">
+        <div className="col-0 col-md-3"></div>
+        <div className="col-12 col-md-6">
+          <Posts Posts={filteredPosts} />
+        </div>
+        <div className="col-0 col-md-3"></div>
       </div>
     </>
   );
@@ -62,8 +167,8 @@ function Header() {
     </div>
   );
 }
-function Search({ onSearch }) {
-  const [searchTerm, SetSearchTerm] = useState("");
+function Search({ onSearch, term }) {
+  const [searchTerm, SetSearchTerm] = useState(term);
   function handleChange(event) {
     SetSearchTerm(event.target.value);
     onSearch(event);
@@ -71,7 +176,12 @@ function Search({ onSearch }) {
   return (
     <>
       <label htmlFor="search">Search Post</label>
-      <input id="search" type="text" onChange={handleChange} />
+      <input
+        id="search"
+        type="text"
+        value={searchTerm}
+        onChange={handleChange}
+      />
     </>
   );
 }
@@ -80,36 +190,36 @@ function Posts({ Posts }) {
   return (
     <>
       {Posts.map((post) => {
-        return <Post post={post} />;
+        return <Post {...post} />;
       })}
     </>
   );
 }
-function Post({ post }) {
+function Post({ Id, title, description, date, likes, comments, author }) {
   return (
     <p
-      key={post.Id}
+      key={Id}
       className="card d-flex flex-column justify-content-start align-items-start"
     >
-      <p className="fs-4 fw-bold">{post.title}</p>
-      <p>{post.description}</p>
+      <p className="fs-5 fw-bold text-start">{title}</p>
+      <p className="fs-6 text-start">{description}</p>
       <div className="d-flex flex-row justify-content-evenly">
         <button className="btn btn-outline-primary">
           <span>
-            {post.likes}
+            {likes}
             <span>&nbsp;{"Likes"}</span>
           </span>
         </button>
         &nbsp;
         <button className="btn btn-outline-primary">
           <span>
-            {post.comments.length}
+            {comments.length}
             <span>&nbsp;{"Comments"}</span>
           </span>
         </button>
       </div>
       <small>
-        {post.author} | {post.date}
+        {author} | {date}
       </small>
     </p>
   );
