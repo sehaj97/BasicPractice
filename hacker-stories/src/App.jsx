be902bc1e893ccaf27797e9909dfcf24fd2c1969
Sehajpreet Singh <sehajmagan@gmail.com>
2023-10-11 18:06:06 -0400

    added state, callback handler and lifted state to handle search

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,35 +1,118 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
+import "./App.css";
+
+const content = {
+  header: {
+    title: "Hacker Stories",
+    secondaryTitle: "Open Forum Board",
+  },
+  Posts: [
+    {
+      title: "How to build a blog app with React and Vite",
+      description: "This is the best tutorial on building a blog app",
+      author: "sehaj",
+      date: "2021-11-3",
+      Id: 1,
+      likes: 0,
+      comments: ["good one", "no one", "i am comment", "am i comment?"],
+    },
+    {
+      title: "Post 2",
+      description: "Can you learn typescript?",
+      author: "magan",
+      date: "2023-11-3",
+      Id: 2,
+      likes: 0,
+      comments: ["good one", "no one", "i am comment", "am i comment?"],
+    },
+  ],
+};
 function App() {
-  const [count, setCount] = useState(0)
-
+  const handleSearchTerm = (event) => {
+    SetDisplaySearchTerm(event.target.value);
+  };
+  const [displaySearchTerm, SetDisplaySearchTerm] = useState("");
+  const filteredPosts = content.Posts.filter((post) => {
+    return post.title.toLowerCase().includes(displaySearchTerm.toLowerCase());
+  });
   return (
     <>
+      <Header />
+      <hr />
+      <div className="d-flex flex-column justify-content-start align-items-start">
+        <Search onSearch={handleSearchTerm} />
+        {displaySearchTerm && <p>Looking for {displaySearchTerm}</p>}
+      </div>
+      <br />
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Posts Posts={filteredPosts} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+function Header() {
+  return (
+    <div>
+      <h1>{content.header.title}</h1>
+      <h2>{content.header.secondaryTitle}</h2>
+    </div>
+  );
+}
+function Search({ onSearch }) {
+  const [searchTerm, SetSearchTerm] = useState("");
+  function handleChange(event) {
+    SetSearchTerm(event.target.value);
+    onSearch(event);
+  }
+  return (
+    <>
+      <label htmlFor="search">Search Post</label>
+      <input id="search" type="text" onChange={handleChange} />
+    </>
+  );
+}
+
+function Posts({ Posts }) {
+  return (
+    <>
+      {Posts.map((post) => {
+        return <Post post={post} />;
+      })}
+    </>
+  );
+}
+function Post({ post }) {
+  return (
+    <p
+      key={post.Id}
+      className="card d-flex flex-column justify-content-start align-items-start"
+    >
+      <p className="fs-4 fw-bold">{post.title}</p>
+      <p>{post.description}</p>
+      <div className="d-flex flex-row justify-content-evenly">
+        <button className="btn btn-outline-primary">
+          <span>
+            {post.likes}
+            <span>&nbsp;{"Likes"}</span>
+          </span>
+        </button>
+        &nbsp;
+        <button className="btn btn-outline-primary">
+          <span>
+            {post.comments.length}
+            <span>&nbsp;{"Comments"}</span>
+          </span>
+        </button>
+      </div>
+      <small>
+        {post.author} | {post.date}
+      </small>
+    </p>
+  );
+}
+
+export default App;
